ce48e67e13df896cdd5d9186f5fe27219be55989
xsthunder <xsthunder@outlook.com>
2019-06-24 10:52:50 +0800

    Create plt-chinese-support.py

---
+++ plot/plt-chinese-support.py
@@ -0,0 +1,4 @@
+# @ see chinese support @see https://blog.csdn.net/jeff_liu_sky_/article/details/54023745
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
