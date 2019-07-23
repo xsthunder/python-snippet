66bbf80e64d8e01cc2ebfe808b668223b8b95496
xsthunder <xsthunder@outlook.com>
2019-07-23 10:49:59 +0800

    Update split-train-test.py

--- data-mining/split-train-test.py
+++ data-mining/split-train-test.py
@@ -5,6 +5,6 @@ df = df_all.copy()
 train_df, test_df = train_test_split(
                                     df,
                                    test_size = 0.3, 
-                                   random_state = SKLEAN_RAMDOM_SEED,
+                                   random_state = SKLEAN_RAMDOM_SEED,# 返回的结果是一样的，但由于模型的初始化训练的结果会有细微差别
                                    stratify = df[y_column_name]
                                     )
