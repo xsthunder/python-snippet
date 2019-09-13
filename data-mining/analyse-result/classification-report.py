0ab7e494ab218a26d71294a3d617d59e1dcf47b6
xsthunder <xsthunder@outlook.com>
2019-09-13 10:21:03 +0800

    Update classification-report.py

--- data-mining/analyse-result/classification-report.py
+++ data-mining/analyse-result/classification-report.py
@@ -17,4 +17,16 @@ outputs are
    micro avg       0.80      0.80      0.80       454
    macro avg       0.75      0.78      0.76       454
 weighted avg       0.82      0.80      0.81       454
+
+              precision    recall  f1-score   support
+
+       False       查准      查全      0.66       116
+        True       查准      查全      0.86       338
+
+   micro avg       0.80      0.80      0.80       454
+   macro avg       0.75      0.78      0.76       454
+weighted avg       0.82      0.80      0.81       454
+
+sensitivity = 正类的查率率
+specifity = 负类的查全率
 """
