84736807d8c1bbaf9b897d6d944d3643a158755d
xsthunder <xsthunder@outlook.com>
2019-12-24 10:22:45 +0800

    Create seqeval_all.py

---
+++ latticeLstm/seqeval_all.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import json
+import pandas as pd
+import seqeval
+from seqeval.metrics import recall_score
+from seqeval.metrics import classification_report
+from seqeval.metrics import f1_score
+from seqeval.metrics import precision_score
+
+files = sys.argv[1:]
+
+def run(filename):
+	print('------------------       %s      ---------------'%filename)
+	with open(filename, 'r') as f:
+	    obj = json.load(f)
+
+
+	y_true=obj['gold_results']
+	y_pred=obj['pred_results']
+
+	pre = precision_score(y_true, y_pred)
+	rec = recall_score(y_true, y_pred)
+	f1 = f1_score(y_true, y_pred)
+
+	df = pd.DataFrame({'percision':[pre], 'recall':[rec], 'f1':[f1]})
+	print(df)
+
+	report = classification_report(y_true, y_pred)
+	print(report)
+
+for filename in files:
+	run(filename)
