235c02e2a989ea6a569655882073a8a66aa7fae6
xsthunder <xsthunder@outlook.com>
2020-10-05 09:11:53 +0800

    Create path.py

---
+++ python/path.py
@@ -0,0 +1,7 @@
+# [pathlib — Object-oriented filesystem paths — Python 3.8.6 documentation](https://docs.python.org/3/library/pathlib.html)
+import os
+import re
+channel_name = argv[2]
+workdir = Path('bulid-by-channel-name/')
+base_path = workdir/channel_name/('static/js')
+files = os.listdir(base_path)
