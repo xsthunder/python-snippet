b65134ed14afe35cd6bc019d30a1d174f9c358dc
xsthunder <xsthunder@outlook.com>
2020-10-14 09:21:07 +0800

    Update path.py

--- python/path.py
+++ python/path.py
@@ -1,9 +1,10 @@
 # [pathlib — Object-oriented filesystem paths — Python 3.8.6 documentation](https://docs.python.org/3/library/pathlib.html)
 import os
 import re
+import sys 
 from pathlib import Path
 
-channel_name = argv[2]
+channel_name = sys.argv[2]
 workdir = Path('bulid-by-channel-name/')
 base_path = workdir/channel_name/('static/js')
 files = os.listdir(base_path)
